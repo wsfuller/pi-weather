dfaa41158d49165c918ae18369226ae9236e4701
Steve Fuller <wsfuller.dev@gmail.com>
2024-12-01 21:45:08 -0800

    Updating styles and adding functionality

--- assets/javascripts/weather.js
+++ assets/javascripts/weather.js
@@ -8,11 +8,6 @@ const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${D
 const DEFAULT_CITY = 'Seattle';
 const DEFAULT_STATE = 'WA';
 const LOCATION_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${DEFAULT_CITY},${DEFAULT_STATE},1&appid=${API_KEY}`;
-let sunriseTime = null;
-let sunsetTime = null;
-
-// console.log('WEATHER_API_URL', WEATHER_API_URL);
-// console.log('LOCATION_API_URL', LOCATION_API_URL);
 
 async function getWeather() {
   const url = WEATHER_API_URL;
@@ -24,67 +19,136 @@ async function getWeather() {
 
     const json = await response.json();
     console.log('json', json);
-    const minTemperature = Math.floor(json.main.temp_min);
-    const mainTemperature = Math.floor(json.main.temp);
-    const maxTemperature = Math.floor(json.main.temp_max);
 
-    document.querySelector('#min-temperature').innerHTML = `${minTemperature}&deg;F`;
+    const mainTemperature = Math.floor(json.main.temp);
+    const minTemperature = Math.floor(json.main.temp_min);
+    const maxTemperature = Math.floor(json.main.temp_max);
+    const feelsLikeTemperature = Math.floor(json.main.feels_like);
+
     document.querySelector('#main-temperature').innerHTML = `${mainTemperature}&deg;F`;
+    document.querySelector('#min-temperature').innerHTML = `${minTemperature}&deg;F`;
+    document.querySelector('#feels-like-temperature').innerHTML = `${feelsLikeTemperature}&deg;F`;
     document.querySelector('#max-temperature').innerHTML = `${maxTemperature}&deg;F`;
 
     const convertedSunrise = convertUnixTime(json.sys.sunrise);
     const convertedSunset = convertUnixTime(json.sys.sunset);
-    const sunriseElement = document.querySelector('#sunrise > .label').innerHTML = convertedSunrise;
-    const sunsetElement = document.querySelector('#sunset > .label').innerHTML = convertedSunset;
+
+    document.querySelector('#sunrise > .label').innerHTML = convertedSunrise;
+    document.querySelector('#sunset > .label').innerHTML = convertedSunset;
 
     const sunriseUnixTime = json.sys.sunrise;
     const sunsetUnixTime = json.sys.sunset;
 
-
-
+    getWeatherIcon(json.weather[0], sunriseUnixTime, sunsetUnixTime);
     setTimeOfDay(sunriseUnixTime, sunsetUnixTime);
 
   } catch (error) {
     console.error(error.message);
+  }
+
+  // Check every hour
+  setTimeout(getWeather, 3600000);
+}
+
+function getWeatherIcon(weather, sunriseUnixTime, sunsetUnixTime) {
+  const iconPath = './assets/icons/';
+  const iconElement = document.querySelector('#weather-icon');
+  const timeNow = Date.now() / 1000;
+
+  switch (weather.main){
+    case 'Clear':
+      if (timeNow > sunriseUnixTime  && timeNow < sunsetUnixTime) {
+        iconElement.src = `${iconPath}clear-day.svg`;
+      } else {
+        iconElement.src = `${iconPath}clear-night.svg`;
+      }
+      iconElement.alt = weather.description;
+      break;
+    case 'Clouds':
+      iconElement.src = `${iconPath}cloudy.svg`;
+      iconElement.alt = weather.description;
+      break;
+    case 'Drizzle':
+      iconElement.src = `${iconPath}drizzle.svg`;
+      iconElement.alt = weather.description;
+      break;
+    case 'Mist':
+      iconElement.src = `${iconPath}mist.svg`;
+      iconElement.alt = weather.description;
+      break;
+    case 'Rain':
+      iconElement.src = `${iconPath}rain.svg`;
+      iconElement.alt = weather.description;
+      break;
+    case 'Snow':
+      iconElement.src = `${iconPath}snow.svg`;
+      iconElement.alt = weather.description;
+      break;
+    case 'Thunderstorm':
+      iconElement.src = `${iconPath}thunderstorms-rain.svg`;
+      iconElement.alt = weather.description;
+      break;
+    default:
+       iconElement.src = `${iconPath}not-available.svg`;
+       iconElement.alt = 'not available';
   }
 }
 
 function setTimeOfDay(sunriseUnixTime, sunsetUnixTime) {
   console.log('get time of day');
   const date = new Date();
-  const unixTime = Math.floor(Date.now() / 1000);
-  const noon = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0);
+  const now = Math.floor(Date.now() / 1000);
+  const noon = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0);
   const noonUnixTime = Math.floor(noon.getTime() / 1000);
   const midnight = new Date(date.getFullYear(), date.getMonth(), date.getDate());
   const midnightUnixTime = Math.floor(midnight.getTime() / 1000);
 
   const oneHour = 3600;
 
-  if(sunriseUnixTime - oneHour ) {
-    console.log('before sunrise');
-    document.body.className = "";
-    document.body.classList.add('night');
-  } else if (unixTime > sunriseUnixTime && unixTime < sunsetUnixTime) {
+  // If daytime
+  if (now <= sunriseUnixTime - oneHour && now < sunsetUnixTime + oneHour) {
     console.log('daytime');
-    document.body.className = "";
-    document.body.classList.add('day');
-  } else if (unixTime > sunsetUnixTime && unixTime < midnightUnixTime) {
-    console.log('evening');
-    document.body.className = "";
-    document.body.classList.add('nighttime');
+    console.log('now', now);
+    // Sunrise +/- 1 hour
+    if (now <= sunriseUnixTime - oneHour || now >= sunriseUnixTime + oneHour) {
+      console.log('sunrise');
+      console.log('now', now);
+      document.body.className = "";
+      document.body.classList.add('sunrise');
+    }
+    // Noon +/- 1 hour
+    if (now >= noonUnixTime - oneHour && now <= noonUnixTime + oneHour) {
+      console.log('noon');
+      console.log('now', now);
+      document.body.className = "";
+      document.body.classList.add('noon');
+    }
+    // Sunset +/- 1 hour
+    if (now >= sunsetUnixTime - oneHour && now <= sunsetUnixTime + oneHour) {
+      console.log('sunset');
+      console.log('now', now);
+      document.body.className = "";
+      document.body.classList.add('sunset');
+    }
   }
-  // console.log('unixTimestamp', unixTimestamp);
+  // If eventing
+  if (now >= sunsetUnixTime + oneHour) {
+    if(now >= midnightUnixTime - oneHour && now <= midnightUnixTime + oneHour) {
+      console.log('midnight');
+      console.log('now', now);
+      document.body.className = "";
+      document.body.classList.add('midnight');
+    } else {
+      console.log('evening');
+      console.log('now', now);
+      document.body.className = "";
+      document.body.classList.add('nighttime');
+    }
 
-  sunrise
-  noon
-  sunset
-  midnight
+  }
 
-  // console.log('hours', hours);
-  // console.log('minutes', minutes);
-  setTimeout(setTimeOfDay, 1000);
-  // // Check every 5 minutes
-  // setTimeout(getTimeOfDay, 300000);
+  // Check every 5 minutes
+  setTimeout(setTimeOfDay, 300000);
 }
 
 function convertUnixTime(unixTime) {
@@ -101,6 +165,4 @@ function convertUnixTime(unixTime) {
 }
 
 getWeather();
-
-
 })();
